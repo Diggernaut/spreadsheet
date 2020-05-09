d4a206a29a3eb2ecab69ebfc42e060920d55ee24
iapon <evgeniysolomanidin@gmail.com>
2020-05-09 17:09:23 +0300

    fixes

--- service.go
+++ service.go
@@ -5,14 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 
+	"github.com/spf13/cast"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
 const (
+	//https://sheets.googleapis.com/v4
 	baseURL = "https://sheets.googleapis.com/v4"
 
 	// Scope is the API scope for viewing and managing your Google Spreadsheet data.
@@ -87,6 +90,57 @@ func (s *Service) FetchSpreadsheet(id string) (spreadsheet Spreadsheet, err erro
 	spreadsheet.service = s
 	return
 }
+func (s *Service) NewSheet(sh *Sheet, l int, name string) (err error) {
+	path := "https://sheets.googleapis.com/v4/spreadsheets/" + sh.Spreadsheet.ID + ":batchUpdate"
+	d := []byte(`
+		[{
+		  "addSheet": {
+			"properties": {
+			  "title": "` + name + `",
+			  "gridProperties": {
+				"rowCount": 0,
+				"columnCount": 0
+			  }
+			}
+		}
+	  }]`)
+	var p = make([]map[string]interface{}, 0)
+	err = json.Unmarshal(d, &p)
+	if err != nil {
+		return err
+	}
+	var e = make(map[string]interface{})
+	e["requests"] = p
+	body, err := sh.Spreadsheet.service.postv4(path, e)
+	if err != nil {
+		return err
+	}
+	fmt.Println(body)
+	return
+}
+func (s *Service) ClearSheet(sh *Sheet) (err error) {
+	path := "https://sheets.googleapis.com/v4/spreadsheets/" + sh.Spreadsheet.ID + ":batchUpdate"
+	d := []byte(`[
+		{
+		  "updateCells": {
+			"range": {
+			  "sheetId": ` + cast.ToString(sh.Properties.ID) + `
+			},
+			"fields": "*"
+		  }
+		}
+	  ]`)
+	var p = make([]map[string]interface{}, 0)
+	err = json.Unmarshal(d, &p)
+	if err != nil {
+		return
+	}
+	var e = make(map[string]interface{})
+	e["requests"] = p
+	body, err := s.postv4(path, e)
+	log.Println(body)
+	return
+}
 
 // SyncSheet updates sheet
 func (s *Service) SyncSheet(sheet *Sheet) (err error) {
@@ -102,8 +156,46 @@ func (s *Service) SyncSheet(sheet *Sheet) (err error) {
 		return
 	}
 	sheet.modifiedCells = []*Cell{}
-	sheet.newMaxRow = sheet.Properties.GridProperties.RowCount
-	sheet.newMaxColumn = sheet.Properties.GridProperties.ColumnCount
+	sheet.Properties.GridProperties.RowCount = sheet.newMaxRow
+	sheet.Properties.GridProperties.ColumnCount = sheet.newMaxColumn
+	return
+}
+func (s *Service) SyncRawSheet(sheet *Sheet, maxrow, maxcol uint, cell []Cell) (err error) {
+	if maxrow > sheet.Properties.GridProperties.RowCount ||
+		maxcol > sheet.Properties.GridProperties.ColumnCount {
+		err = s.ExpandSheet(sheet, maxrow, maxcol)
+		if err != nil {
+			return
+		}
+	}
+	err = s.syncRawCells(sheet, cell)
+	if err != nil {
+		return
+	}
+	sheet.modifiedCells = []*Cell{}
+	sheet.Properties.GridProperties.RowCount = sheet.newMaxRow
+	sheet.Properties.GridProperties.ColumnCount = sheet.newMaxColumn
+	return
+}
+func (s *Service) syncRawCells(sheet *Sheet, cell []Cell) (err error) {
+	path := fmt.Sprintf("/spreadsheets/%s/values:batchUpdate", sheet.Spreadsheet.ID)
+	log.Println(path)
+	params := map[string]interface{}{
+		"valueInputOption": "USER_ENTERED",
+		"data":             make([]map[string]interface{}, 0, len(cell)),
+	}
+	for _, cell := range cell {
+		params["data"] = append(params["data"].([]map[string]interface{}), map[string]interface{}{
+			"range":          sheet.Properties.Title + "!" + cell.Pos(),
+			"majorDimension": "COLUMNS",
+			"values": [][]string{
+				[]string{
+					cell.Value,
+				},
+			},
+		})
+	}
+	_, err = s.post(path, params)
 	return
 }
 
@@ -178,28 +270,51 @@ func (s *Service) get(path string) (body []byte, err error) {
 		return
 	}
 	body, err = ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
 	if err != nil {
 		return
 	}
+	resp.Body.Close()
 	err = s.checkError(body)
 	return
 }
-
-func (s *Service) post(path string, params map[string]interface{}) (body string, err error) {
+func (s *Service) postv4(path string, params interface{}) (body string, err error) {
 	reqBody, err := json.Marshal(params)
 	if err != nil {
 		return
 	}
+	resp, err := s.client.Post(path, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		return
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	resp.Body.Close()
+	err = s.checkError(bytes)
+	if err != nil {
+		return
+	}
+	body = string(bytes)
+	return
+}
+func (s *Service) post(path string, params interface{}) (body string, err error) {
+	reqBody, err := json.Marshal(params)
+	if err != nil {
+		return
+	}
+	log.Println("HERE")
+	// log.Printf("%v", s.client)
+	log.Println(s.client.Timeout.String())
 	resp, err := s.client.Post(baseURL+path, "application/json", bytes.NewReader(reqBody))
 	if err != nil {
 		return
 	}
 	bytes, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
 	if err != nil {
 		return
 	}
+	resp.Body.Close()
 	err = s.checkError(bytes)
 	if err != nil {
 		return
